7649ee0d153232ff83685c946959b132545126b4
youssefhaimerdev <youssef.haimer.pro@gmail.com>
2026-06-03 11:17:54 +0100

    v18

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   eslint: { ignoreDuringBuilds: true },
-=======
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
->>>>>>> 24746b0325c27efd1a9b0ef23f61491f6a07c63d
 };
 module.exports = nextConfig;
